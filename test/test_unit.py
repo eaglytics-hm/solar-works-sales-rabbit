80f69809868194c814f3e87dc0b9a78350af1fe9
HM <hieumdd@gmail.com>
2021-12-27 21:15:38 +0700

    feat: Added service & main.py

--- test/test_unit.py
+++ test/test_unit.py
@@ -1,33 +1,57 @@
+from unittest.mock import Mock
+
 import pytest
 
-from pipeline import pipeline, pipeline_controller, pipeline_repo
-
-pipelines = [
-    pipeline.leads,
-    pipeline.lead_status_histories,
-]
+from pipeline import pipeline, pipeline_service, pipeline_repo
+from main import main
 
 
-@pytest.mark.parametrize(
-    "pl",
-    pipelines,
-    ids=[i.table for i in pipelines],
+@pytest.fixture(
+    params=[
+        pipeline.Leads,
+        pipeline.LeadStatusHistories,
+    ],
+    ids=[
+        pipeline.Leads.table,
+        pipeline.LeadStatusHistories.table,
+    ],
 )
+def pl(request):
+    return request.param
+
+
+@pytest.fixture(
+    params=[
+        None,
+        "2021-12-01",
+    ],
+    ids=[
+        "auto",
+        "manual",
+    ],
+)
+def start(request):
+    return request.param
+
+
 def test_get_latest(pl):
     res = pipeline_repo.get_latest(pl.table, pl.incre_key)()
     assert res
 
 
-@pytest.mark.parametrize(
-    "pl",
-    pipelines,
-    ids=[i.table for i in pipelines],
-)
-@pytest.mark.parametrize(
-    "start",
-    [None, "2021-12-01"],
-    ids=["auto", "manual"],
-)
-def test_pipeline(pl, start):
-    res = pipeline_controller.pipeline_controller(pl)(start)
+def test_pipeline_service(pl, start):
+    res = pipeline_service.pipeline_service(pl)(start)
+    assert res
+
+
+def test_factory(pl):
+    assert pipeline_service.factory(pl.table) == pl
+
+
+def test_pipeline_controller(pl, start):
+    data = {
+        "table": pl.table,
+        "start": start,
+    }
+    res = main(Mock(get_json=Mock(return_value=data), args=data))
     assert res
